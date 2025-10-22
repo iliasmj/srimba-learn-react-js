629c52f71181b4934d1d74e691c0ba7ce1071457
iliasmj <iliasmj@gmail.com>
2025-10-22 23:52:45 +0200

    final commit

--- react-sandbox/src/components/Navbar.jsx
+++ react-sandbox/src/components/Navbar.jsx
@@ -1,5 +1,23 @@
+/**
+ * Challenge: complete the Navbar to match the Figma design 
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+
 export default function Navbar() {
     return(
-        <h1>Navbar goes here</h1>
+        <header className="header">
+            <nav className="header-nav">
+                <div className="header-nav-logo">
+                    <img className="header-nav-logo-img" src="/src/assets/react-logo.png" alt="React Logo"/>
+                    <p className="header-nav-logo-p">ReactFacts</p>
+                </div>
+            </nav>
+        </header>
     )
 }
